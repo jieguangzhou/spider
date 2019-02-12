81f25fd19753f1a50b4e442b078fdc2dc31fda12
jieguangzhou <jieguang_zhou@163.com>
2019-02-12 23:32:09 +0800

    添加爬去汽车之家车型配置

--- car/car/spiders/spider_autohome_car_config.py
+++ car/car/spiders/spider_autohome_car_config.py
@@ -5,54 +5,94 @@ from bs4 import BeautifulSoup
 from ..items import CarConfigItem
 import re
 from pymongo import MongoClient
+import logging
+import json
+import PyV8
 
+ctx = PyV8.JSContext()
+ctx.enter()
 
 class AutoHomeCarSpider(Spider):
     name = "autohome_car_config"
 
     def start_requests(self):
-        # mongo_host = settings['MONGO_HOST']
-        # mongo_port = settings['MONGO_PORT']
-        # mongo_db = settings['MONGO_DB']
-        # clct_car = MongoClient(mongo_host, mongo_port)[mongo_db]['car']
-        # for car in clct_car.find():
-        #
-        #     url = car['car_info_url']
-        #     request = SplashRequest(url, callback=self.parse)
-        #     request.meta.update(car)
-        #     yield request
+        mongo_host = settings['MONGO_HOST']
+        mongo_port = settings['MONGO_PORT']
+        mongo_db = settings['MONGO_DB']
+        clct_car = MongoClient(mongo_host, mongo_port)[mongo_db]['car']
+        for car in clct_car.find():
+
+            url = car['car_info_url']
+            request = SplashRequest(url, callback=self.parse)
+            request.meta.update(car)
+            yield request
 
 
-        url = 'https://car.autohome.com.cn/config/spec/32040.html'
-        request = SplashRequest(url, callback=self.parse)
-        request.meta['id'] = '32040'
-        yield request
+        # url = 'https://car.autohome.com.cn/config/spec/32040.html'
+        # request = SplashRequest(url, callback=self.parse)
+        # request.meta['id'] = '32040'
+        # yield request
 
     def parse(self, response):
-        bs = BeautifulSoup(response.body.decode('utf-8'), 'lxml')
+        text = response.body.decode('utf-8')
         car_data = response.meta
-        tables = bs.select('table > tbody')
-        tables_cfg = {}
-        for idx, table in enumerate(tables):
+        config = get_config(text)['result']['paramtypeitems']
 
-            trs = table.select('tr[id|=tr]')
-            table_name_ = table.select('h3 > span')
-            if table_name_:
-                table_name = table_name_[0].text
-            else:
-                table_name = idx
-            table_cfg = {}
-            for tr in trs:
-                key = tr.text
-                value = tr.select('td')[0].text
-                table_cfg[key] = value
-
-            tables_cfg[table_name] = table_cfg
-
-        item_config = CarConfigItem(id=car_data['id'], config=tables_cfg)
+        item_config = CarConfigItem(id=car_data['id'], config=config)
 
         yield item_config
 
 
         self.logger.info(car_data['id'])
 
+
+def get_config(text):
+    alljs = makejs(text)
+    config = re.findall("var config = (.*?);\n", text, re.DOTALL)[0]
+    ctx.eval(alljs)
+    result = str(ctx.eval('rules'))
+    mapping = [extract(i) for i in result.split('#')]
+    for key, value in mapping:
+        config = config.replace(key, value)
+    return json.loads(config)
+
+
+
+def extract(string):
+    key = "<span class='" + string[1: string.index(':')] + "'></span>"
+    value = re.sub('.*content:"(.*?)".*', '\\1', string)
+    return key, value
+
+def makejs(html):
+    try:
+        alljs = ("var rules = '';"
+                 "var document = {};"
+                 "document.createElement = function() {"
+                 "      return {"
+                 "              sheet: {"
+                 "                      insertRule: function(rule, i) {"
+                 "                              if (rules.length == 0) {"
+                 "                                      rules = rule;"
+                 "                              } else {"
+                 "                                      rules = rules + '#' + rule;"
+                 "                              }"
+                 "                      }"
+                 "              }"
+                 "      }"
+                 "};"
+                 "document.querySelectorAll = function() {"
+                 "      return {};"
+                 "};"
+                 "document.head = {};"
+                 "document.head.appendChild = function() {};"
+
+                 "var window = {};"
+                 "window.decodeURIComponent = decodeURIComponent;")
+
+        js = re.findall('(\(function\([a-zA-Z]{2}.*?_\).*?\(document\);)', html)
+        for item in js:
+            alljs = alljs + item
+        return alljs
+    except:
+        logging.exception('makejs function exception')
+        return None
